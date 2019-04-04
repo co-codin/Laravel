26627f8062eba9a1c89d092b6987b7b493cde3de
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-04 18:30:15 +0300

    put all data to elasticsearch server

--- ElasticSearchDriver/app/Search/Engine/ElasticSearchEngine.php
+++ ElasticSearchDriver/app/Search/Engine/ElasticSearchEngine.php
@@ -23,7 +23,16 @@ class ElasticSearchEngine extends Engine
      */
     public function update($models)
     {
-        dd($this->client);
+        $models->each(function ($model) {
+            $params = [
+                'index' => $model->searchableAs(),
+                'type' => $model->searchableAs(),
+                'id' => $model->id,
+                'body' => $model->toSearchableArray()
+            ];
+
+            $this->client->index($params);
+        });
     }
 
     /**
--- ElasticSearchDriver/app/User.php
+++ ElasticSearchDriver/app/User.php
@@ -37,4 +37,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function toSearchableArray()
+    {
+        return [
+            'username' => $this->username,
+            'name' => $this->name
+        ];
+    }
 }
