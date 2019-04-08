e22d09e1e84c0e9d57bc2fc8a9c73237c32cb340
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-08 21:59:27 +0300

    packages version

--- FileMarket/resources/views/layouts/partials/_navigation.blade.php
+++ FileMarket/resources/views/layouts/partials/_navigation.blade.php
@@ -1,1 +1,47 @@
-Nav
+<nav class="nav">
+    <div class="container">
+        <div class="nav-left">
+            <a href="{{ route('home') }}" class="nav-item is-brand">
+                {{ config('app.name') }}
+            </a>
+        </div>
+
+        <span class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+
+        <div class="nav-right nav-menu">
+            @if(auth()->check())
+                <a href="#" class="nav-item" onclick="event.preventDefault(); document.getElementById('logout').submit();">
+                    Sign out
+                </a>
+
+                <a href="{{ route('account') }}" class="nav-item">
+                    Your account
+                </a>
+
+                @role('admin')
+                    <a href="{{ route('admin.index') }}" class="nav-item">
+                        Admin
+                    </a>
+                @endrole
+            @else
+                <a href="{{ route('login') }}" class="nav-item">
+                    Sign in
+                </a>
+
+                <div class="nav-item">
+                    <a href="{{ route('register') }}" class="button">
+                        Start selling
+                    </a>
+                </div>
+            @endif
+        </div>
+    </div>
+</nav>
+
+<form id="logout" action="{{ route('logout') }}" method="POST" class="is-hidden">
+    {{ csrf_field() }}
+</form>
