b063399e22084b4777fd04548f2844aca359ff41
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-08 18:42:19 +0300

    logout method

---
+++ LaravelNuxtAuth/api/app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LogoutController extends Controller
+{
+    public function logout()
+    {
+        auth()->logout();
+    }
+}
--- LaravelNuxtAuth/api/routes/api.php
+++ LaravelNuxtAuth/api/routes/api.php
@@ -2,5 +2,6 @@
 
 Route::post('/register', 'Auth\RegisterController@register');
 Route::post('/login', 'Auth\LoginController@login');
+Route::post('/logout', 'Auth\LogoutController@logout');
 
 Route::get('/me', 'Auth\MeController@me');
