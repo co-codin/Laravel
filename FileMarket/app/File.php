1eab19f824667c61c5290e383d132e4dcf635af9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-09 10:13:05 +0300

    user-file

--- FileMarket/app/File.php
+++ FileMarket/app/File.php
@@ -2,9 +2,31 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class File extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'overview_short',
+        'overview',
+        'price',
+        'live',
+        'approved',
+        'finished',
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'identifier';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
