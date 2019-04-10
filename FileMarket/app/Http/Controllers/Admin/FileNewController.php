6f8615e7aa320ad7d2a679baa873fa083e99f68e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-10 11:49:32 +0300

    sending emails

--- FileMarket/app/Http/Controllers/Admin/FileNewController.php
+++ FileMarket/app/Http/Controllers/Admin/FileNewController.php
@@ -6,6 +6,7 @@ use Mail;
 use App\File;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Mail\Files\{FileApproved, FileRejected};
 
 class FileNewController extends Controller
 {
@@ -20,6 +21,8 @@ class FileNewController extends Controller
     {
         $file->approve();
 
+        Mail::to($file->user)->send(new FileApproved($file));
+
         return back()->withSuccess("{$file->title} has been approved");
     }
 
@@ -27,6 +30,8 @@ class FileNewController extends Controller
     {
         $file->delete();
         $file->uploads->each->delete();
+
+        Mail::to($file->user)->send(new FileRejected($file));
 
         return back()->withSuccess("{$file->title} has been rejected");
     }
