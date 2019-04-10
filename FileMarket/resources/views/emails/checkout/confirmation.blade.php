7a7f81b22fce1fc821b4061f35a7bff9383d8bbc
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-10 15:16:41 +0300

    preparing to download file in email

--- FileMarket/resources/views/emails/checkout/confirmation.blade.php
+++ FileMarket/resources/views/emails/checkout/confirmation.blade.php
@@ -1,1 +1,11 @@
-big
+@extends('emails.layouts.default')
+
+@section('content')
+    <p>Thanks for downloading <strong>{{ $sale->file->title }}</strong> from Filemarket.</p>
+
+    <p><a href="">Download your file</a></p>
+
+    <p>
+        Or, copy and paste this into your browser:<br>
+    </p>
+@endsection
