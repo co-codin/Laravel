f35a952aaa6da378e66b81facf50cbf7407ce7a4
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-16 20:50:09 +0300

    save a comment

---
+++ ReusableComment/app/Http/Controllers/Comments/CommentReplyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Comments;
+
+use App\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CommentResource;
+
+class CommentReplyController extends Controller
+{
+    public function store(Comment $comment, Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required|max:5000'
+        ]);
+
+        $reply = $comment->children()->make([
+            'body' => $request->body
+        ]);
+
+        $reply->commentable()->associate($comment->commentable);
+
+        $request->user()->comments()->save($reply);
+
+        return new CommentResource($reply);
+    }
+}
--- ReusableComment/routes/web.php
+++ ReusableComment/routes/web.php
@@ -8,6 +8,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::resource('comments/{comment}/replies', 'Comments\CommentReplyController');
+
 Route::resource('courses', 'CourseController');
 Route::resource('courses/{course}/comments', 'Courses\CourseCommentController', [
     'names' => [
