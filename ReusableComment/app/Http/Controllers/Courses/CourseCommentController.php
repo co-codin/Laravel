22da3f5d9e0b487f664ac844d460287b77e6acc1
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-16 12:33:41 +0300

    sotre a comment

--- ReusableComment/app/Http/Controllers/Courses/CourseCommentController.php
+++ ReusableComment/app/Http/Controllers/Courses/CourseCommentController.php
@@ -13,4 +13,19 @@ class CourseCommentController extends Controller
     {
         return CommentResource::collection($course->comments);
     }
+
+    public function store(Course $course, Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required|max:5000'
+        ]);
+
+        $comment = $course->comments()->make([
+            'body' => $request->body
+        ]);
+
+        $request->user()->comments()->save($comment);
+
+        return new CommentResource($comment);
+    }
 }
