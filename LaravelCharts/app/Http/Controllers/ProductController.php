7728b207dd7c24b0f1d607c4d8aaae77c75ca300
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-25 12:52:20 +0300

    product controller built

--- LaravelCharts/app/Http/Controllers/ProductController.php
+++ LaravelCharts/app/Http/Controllers/ProductController.php
@@ -9,11 +9,18 @@ class ProductController extends Controller
 {
     public function index()
     {
-
+        return response()->json(
+            Product::orderBy('year', 'ASC')->get()
+        );
     }
 
-    public function store()
+    public function store(Request $request)
     {
-
+        $product = new Product();
+        $product->name = $request->name;
+        $product->year = $request->year;
+        $product->price = $request->price;
+        $product->save();
+        return response()->json(['success'=>'The product was created succesfully']);
     }
 }
--- LaravelCharts/routes/web.php
+++ LaravelCharts/routes/web.php
@@ -2,3 +2,7 @@
 
 Route::get('/products', 'ProductController@index');
 Route::post('/products', 'ProductController@store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
