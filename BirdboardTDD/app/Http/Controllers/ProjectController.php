82c2a49a474449d206fa66e011ad9daa67affbf8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-28 15:25:19 +0300

    building Project model controller migration and tests

---
+++ BirdboardTDD/app/Http/Controllers/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+        
+    }
+}
---
+++ BirdboardTDD/database/factories/ProjectFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Project;
+use Faker\Generator as Faker;
+
+$factory->define(Project::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(4),
+        'description' => $faker->sentence(4),
+        'owner_id' => function () {
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
