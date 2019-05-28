a0f872e25ec4957a3aaf82ddebbe97cadeeb85ee
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-28 23:58:58 +0300

    showing tasks

--- BirdboardTDD/app/Http/Controllers/ProjectTasksController.php
+++ BirdboardTDD/app/Http/Controllers/ProjectTasksController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Project;
 
 class ProjectTasksController extends Controller
 {
-    //
+    public function store(Project $project)
+    {
+        request()->validate(['body' => 'required']);
+
+        $project->addTask(request('body'));
+        
+        return redirect($project->path());
+    }
 }
