92e12422c350c053a977b64cdeadfa7cbadbe0a5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-28 23:49:04 +0300

    auth func refactored

--- BirdboardTDD/tests/Feature/ProjectTasksTest.php
+++ BirdboardTDD/tests/Feature/ProjectTasksTest.php
@@ -9,16 +9,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ProjectTasksTest extends TestCase
 {
     use RefreshDatabase;
-    
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_project_can_have_tasks()
+    {
+
     }
 }
