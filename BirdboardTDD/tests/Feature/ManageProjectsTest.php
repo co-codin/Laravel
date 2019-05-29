447d2ab101f7f5de47607ff9e725a852f8ad1724
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-29 14:33:56 +0300

    making project policy

--- BirdboardTDD/tests/Feature/ManageProjectsTest.php
+++ BirdboardTDD/tests/Feature/ManageProjectsTest.php
@@ -46,6 +46,22 @@ class ManageProjectsTest extends TestCase
     }
 
     /** @test */
+    function a_user_can_update_a_project()
+    {
+        $this->signIn();
+
+        $this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
+
+        $this->patch($project->path(), [
+            'notes' => 'Changed'
+        ])->assertRedirect($project->path());
+
+        $this->assertDatabaseHas('projects', ['notes' => 'Changed']);
+    }
+
+    /** @test */
     public function a_user_can_view_their_project()
     {
         $this->signIn();
@@ -57,6 +73,16 @@ class ManageProjectsTest extends TestCase
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_update_the_projects_of_others()
+    {
+        $this->signIn();
+        
+        $project = factory('App\Project')->create();
+        
+        $this->patch($project->path(), [])->assertStatus(403);
     }
 
     /** @test */
