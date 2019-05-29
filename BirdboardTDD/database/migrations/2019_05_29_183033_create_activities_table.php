fd888191031684f123ff54e5e2b7c638c2272274
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-29 21:40:21 +0300

    recording activities

--- BirdboardTDD/database/migrations/2019_05_29_183033_create_activities_table.php
+++ BirdboardTDD/database/migrations/2019_05_29_183033_create_activities_table.php
@@ -15,7 +15,11 @@ class CreateActivitiesTable extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('project_id');
+            $table->string('description');
             $table->timestamps();
+
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
         });
     }
 
--- BirdboardTDD/tests/Feature/ActivityFeedTest.php
+++ BirdboardTDD/tests/Feature/ActivityFeedTest.php
@@ -3,18 +3,31 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ActivityFeedTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    /** @test */
+    function creating_a_project_generates_activity()
     {
-        $this->assertTrue(true);
+        $project = ProjectFactory::create();
+
+        $this->assertCount(1, $project->activity);
+        $this->assertEquals('created', $project->activity[0]->description);
+    }
+
+    /** @test */
+    function updating_a_project_generates_activity()
+    {
+        $project = ProjectFactory::create();
+
+        $project->update(['title' => 'Changed']);
+
+        $this->assertCount(2, $project->activity);
+        
+        $this->assertEquals('updated', $project->activity->last()->description);
     }
 }
