7359f575dbf82858545c6e584e3d5e31fa0e1fa9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-05 16:16:22 +0300

    invitation tests built

--- BirdboardTDD/tests/Unit/ProjectTest.php
+++ BirdboardTDD/tests/Unit/ProjectTest.php
@@ -35,4 +35,14 @@ class ProjectTest extends TestCase
         $this->assertCount(1, $project->tasks);
         $this->assertTrue($project->tasks->contains($task));
     }
+
+    /** @test */
+    function it_can_invite_a_user()
+    {
+        $project = factory('App\Project')->create();
+        
+        $project->invite($user = factory(User::class)->create());
+        
+        $this->assertTrue($project->members->contains($user));
+    }
 }
