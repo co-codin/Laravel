2ff2e62a0deeaf707cdf931727e84ed13def54c9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-05 14:57:28 +0300

    activity cards showing

---
+++ BirdboardTDD/tests/Unit/ActivityTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\{Activity, Project, User};
+use Facades\Tests\Setup\ProjectFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ActivityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function it_has_a_user()
+    {
+        $user = $this->signIn();
+
+        $project = ProjectFactory::ownedBy($user)->create();
+
+        $this->assertEquals($user->id, $project->activity->first()->user->id);
+    }
+}
