e1028a2929ebd46736147cfe7c6a7b87634a9d85
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-06 18:10:00 +0300

    project invites users

--- BirdboardTDD/tests/Feature/InvitationsTest.php
+++ BirdboardTDD/tests/Feature/InvitationsTest.php
@@ -11,6 +11,43 @@ class InvitationsTest extends TestCase
 {
     use RefreshDatabase;
 
+    function non_owners_may_not_invite_users()
+    {
+        $this->actingAs(factory(User::class)->create())
+            ->post(ProjectFactory::create()->path() . '/invitations')
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    function a_project_owner_can_invite_a_user()
+    {
+        $project = ProjectFactory::create();
+
+        $userToInvite = factory(User::class)->create();
+
+        $this->actingAs($project->owner)
+             ->post($project->path() . '/invitations', [
+                 'email' => $userToInvite->email
+             ])
+             ->assertRedirect($project->path());
+
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test */
+    function the_email_address_must_be_associated_with_a_valid_birdboard_account()
+    {
+        $project = ProjectFactory::create();
+
+        $this->actingAs($project->owner)
+             ->post($project->path() . '/invitations', [
+                 'email' => 'notauser@example.com'
+             ])
+             ->assertSessionHasErrors([
+                 'email' => 'The user you are inviting must have a Birdboard account.'
+             ]);
+    }
+
     /** @test */
     function a_project_can_invite_a_user()
     {
