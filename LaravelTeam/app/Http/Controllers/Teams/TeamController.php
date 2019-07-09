8475a11ea001fb4dc9c14e012f130cb6195dd46f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-09 16:57:56 +0300

    building Team controller model

---
+++ LaravelTeam/app/Http/Controllers/Teams/TeamController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Teams;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TeamController extends Controller
+{
+    public function index()
+    {
+        return view('teams.index');
+    }
+}
