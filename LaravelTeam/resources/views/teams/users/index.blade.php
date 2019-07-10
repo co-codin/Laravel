5c59352384fe0fc318e1cefc9a93a592ae1b1332
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-10 14:22:43 +0300

    listing team users

--- LaravelTeam/resources/views/teams/users/index.blade.php
+++ LaravelTeam/resources/views/teams/users/index.blade.php
@@ -12,7 +12,36 @@
                 </div>
 
                 <div class="card-body">
-                    
+                    <table class="table mb-0">
+                        <thead>
+                            <tr>
+                                <th>User</th>
+                                <th>Role</th>
+                                <th>Joined</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($team->users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>
+                                        @if ($team->ownedBy($user))
+                                            <span class="badge badge-primary badge-pill">Admin</span>
+                                        @else
+                                            <span class="badge badge-primary badge-pill">Member</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        {{ $user->pivot->created_at }}
+                                    </td>
+                                    <td>
+                                        Menu
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
