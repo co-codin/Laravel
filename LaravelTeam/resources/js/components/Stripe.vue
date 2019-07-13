74e2ff882cc8e5d5ad885f51c80df635789c1245
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-13 11:35:46 +0300

    stripe card form UI, doing event-listener

--- LaravelTeam/resources/js/components/Stripe.vue
+++ LaravelTeam/resources/js/components/Stripe.vue
@@ -1,11 +1,34 @@
 <template>
     <div>
-        Stripe
+        <div id="card-element" class="mb-2"></div>
+        <div id="card-errors"></div>
+
+        <input type="hidden" name="token" :value="token">
     </div>
 </template>
 
 <script>
     export default {
-        
+        mounted () {
+            let stripe = Stripe('pk_test_o2qLuru9np9gu9tch9Vn3MtJ')
+            let elements = stripe.elements()
+
+            var card = elements.create('card', {
+                style: {
+                    base: {
+                        fontSize: '14px',
+                        color: '#32325d'
+                    }
+                }
+            })
+
+            card.addEventListener('change', (event) => {
+                let displayError = document.getElementById('card-errors')
+
+                
+            })
+
+            card.mount('#card-element')
+        }
     }
 </script>
--- LaravelTeam/resources/views/layouts/app.blade.php
+++ LaravelTeam/resources/views/layouts/app.blade.php
@@ -10,19 +10,19 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://js.stripe.com/v3/"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
