d4b9c8223cdb8b595648099821b8b89165aeb726
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-13 22:08:24 +0300

    scroll built

--- PracticalVueComponents/resources/js/app.js
+++ PracticalVueComponents/resources/js/app.js
@@ -1,3 +1,4 @@
+import ScrollLink from './components/ScrollLink';
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -19,8 +20,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.component('scroll-link', ScrollLink);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
