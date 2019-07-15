0fb6c1ecff338dabe028bb4d8d88945004edb72c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-15 13:43:44 +0300

    building confirm button

---
+++ PracticalVueComponents/resources/js/components/ConfirmButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="confirm">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            message: {},
+            confirmButton: { default: 'Continue' },
+            cancelButton: { default: 'Cancel' }
+        },
+
+        data () {
+            return {
+                confirmed: false
+            }
+        },
+
+        methods: {
+            
+        }
+    }
+</script>
