5d310f517642f6c1b29a4f79eced06100573e9d5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-23 23:22:36 +0300

    seeder built

--- LaravelGraphQL/database/seeds/DatabaseSeeder.php
+++ LaravelGraphQL/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Project;
+use App\Task;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::truncate();
+        Project::truncate();
+        Task::truncate();
+        DB::table('project_user')->truncate();
+
+        $admin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('hellojava'),
+            'api_token' => Str::random(60),
+        ]);
+
+        $user1 = User::create([
+            'name' => 'elijah',
+            'email' => 'elijah@gmail.com',
+            'password' => bcrypt('hellojava'),
+            'api_token' => Str::random(60),
+        ]);
+
+        $project = Project::create([
+            'title' => 'Project Manager Development',
+            'description' => 'Write the project manager application',
+            'manager_id' => $admin->id
+        ]);
+
+        $task1 = Task::create([
+            'title' => 'Seed Database',
+            'description' => 'Seed the database with test data.',
+            'user_id' => $admin->id,
+            'project_id' => $project->id,
+            'status_code' => 'COMP',
+        ]);
+
+        $task2 = Task::create([
+            'title' => 'Complete Developmnet',
+            'description' => 'Write the code yo.',
+            'user_id' => $user1->id,
+            'project_id' => $project->id,
+            'status_code' => 'OPEN',
+        ]);
+
+        $project->users()->saveMany([$admin, $user1]);
     }
 }
