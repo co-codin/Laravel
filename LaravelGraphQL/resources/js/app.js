34920b6b371d1bda73f20a7281d5c1b3f05fb4fb
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-24 12:55:54 +0300

    basic template built, building routes

--- LaravelGraphQL/resources/js/app.js
+++ LaravelGraphQL/resources/js/app.js
@@ -1,6 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './components/App.vue';
+import routes from './routes';
+
 require('./bootstrap');
 
+Vue.use(VueRouter);
+
+const router = new VueRouter();
 
 const app = new Vue({
-    el: '#app',
-});
+    router,
+    render: h=> h(App)
+}).$mount('#app');
---
+++ LaravelGraphQL/resources/js/components/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    Project Management
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link class="nav-link">Dashboard</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link">Create Project</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Logout</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link class="nav-link">Login</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link">Register</router-link>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+        <section id="body-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
