bbdca4f3c00cd0c2053a6f5a99839a9e7901d4e3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-13 13:30:26 +0300

    registering queries controller

---
+++ GraphQL/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\User;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $faker = Factory::create();
+
+        User::create([
+            'name' => 'Elijah',
+            'email' => 'elijah@gmail.com',
+            'password' => bcrypt('secret')
+        ]);
+
+        foreach (range(1, 10) as $i) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => str_random()
+            ]);
+        }
+    }
+}
