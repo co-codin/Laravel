3c8c6d487876e908d9f0ba00e7d9738fd4125b7a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-15 21:37:17 +0300

    db schemas set up

--- RebingGraphQL/database/migrations/2019_09_15_183253_create_tasks_table.php
+++ RebingGraphQL/database/migrations/2019_09_15_183253_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('api_token')->unique();
-            $table->rememberToken();
+            $table->text('title');
+            $table->text('description');
+            $table->text('status_code');
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 }
