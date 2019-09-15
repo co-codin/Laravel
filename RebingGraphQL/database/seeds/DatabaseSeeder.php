83ceac61b09a8b4139930fe106b6cf3c55a56e88
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-15 21:47:10 +0300

    fake data

--- RebingGraphQL/database/seeds/DatabaseSeeder.php
+++ RebingGraphQL/database/seeds/DatabaseSeeder.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Post;
+use Illuminate\Support\Facades\DB;
+use App\{
+    Post, User, Project, Task
+};
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +16,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(Post::class, 20)->create();
+        // factory(Post::class, 20)->create();
         // $this->call(UsersTableSeeder::class);
+
+        User::truncate();
+        Project::truncate();
+        Task::truncate();
+        DB::table('project_user')->truncate();
+
+        $admin = User::create([
+            'name' => 'elijah',
+            'email' => 'elijah@gmail.com',
+            'password' => bcrypt('hellojava'),
+            'api_token' => Str::random(60)
+        ]);
+
+        $user1 = User::create([
+            'name' => 'ivan',
+            'email' => 'ivan@gmail.com',
+            'password' => bcrypt('hellojava'),
+            'api_token' => Str::random(60)
+        ]);
+
+        $project = Project::create([
+            'title' => 'test title',
+            'description' => 'test description',
+            'manager_id' => $admin->id
+        ]);
+
+        $task1 = Task::create([
+            'title' => 'test title',
+            'description' => 'test description',
+            'user_id' => $admin->id,
+            'project_id' => $project->id,
+            'status_code' => 'COMP'
+        ]);
+
+        $task2 = Task::create([
+            'title' => 'test title 2',
+            'description' => 'test description 2',
+            'user_id' => $user1->id,
+            'project_id' => $project->id,
+            'status_code' => 'OPEN'
+        ]);
+
+        $project->users()->saveMany([$admin, $user1]);
     }
 }
