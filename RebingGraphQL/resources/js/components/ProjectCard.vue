bff7d2b8c42334cc1080c51746af8690d87128be
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-16 13:09:10 +0300

    show projects

--- RebingGraphQL/resources/js/components/ProjectCard.vue
+++ RebingGraphQL/resources/js/components/ProjectCard.vue
@@ -2,11 +2,9 @@
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">
-                <router-link :to="`/project/${project.id}`">
-                    {{project.title}}
-                </router-link>
+                {{ project.title }}
             </h5>
-            <p class="card-text">{{project.description}}</p>
+            <p class="card-text">{{ project.description }}</p>
         </div>
     </div>
 </template>
