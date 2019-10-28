86edb5c5fa250355da2bfc8238369909b6d16cf3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-28 22:48:34 +0300

    doing nuxt instant search

---
+++ NuxtInstantSearch/components/SomeComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-wrap lg:flex-no-wrap justify-between items-center bg-white p-8 rounded w-full mb-2">
+        <div class="mr-8 mb-2 lg:mb-0">
+            <h1 class="text-gray-600 text-xl leading-snug">
+                <a href="#" class="text-gray-600">
+                    {{ item.name }}
+                </a>
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            item: {
+                required: true,
+                type: Object
+            }
+        }
+    }
+</script>
---
+++ NuxtInstantSearch/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container py-20">
+		<div class="mb-10">
+			<div class="flex justify-between items-end mb-6">
+				<h1 class="text-2xl text-gray-700 leading-tight">
+					Search
+				</h1>
+			</div>
+		</div>
+    </div>
+</template>
+
+<script>
+	export default {
+		//
+	}
+</script>
